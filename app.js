332e59941316a3257747a1c8d879e4e8d13306f1
Felipe Fezzi <felipe.alves.fezziu@gmail.com>
2025-05-22 09:26:43 -0300

    Voltando o limite para 5

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do núumero secreto');
-let numeroSecreto = 10;
+let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto) {
     console.log('Isso ai! Você descobriu o número secreto (5)');
