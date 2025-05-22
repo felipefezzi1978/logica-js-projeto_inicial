5a4d0f825281997a8eb66fe398ce16e206360102
Felipe Fezzi <felipe.alves.fezziu@gmail.com>
2025-05-22 12:09:19 -0300

    Inclusão de condicionais e console logs

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 alert('Boas vindas ao jogo do núumero secreto');
-let numeroSecreto = 5;
+
+let numeroSecreto = 9;
+console.log(`Valor do numero secreto: ${numeroSecreto}`); // Dev acompanhar qual é o número secreto
+
 let chute = prompt('Escolha um número entre 1 e 10');
+console.log(`Valor do chute: ${chute}`); // Dev acompanhar qual é o chute
+
+
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    // se o chute for igual ao número secreto
+        console.log(`Valor da comparação: ${chute == numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        
+} else {
+    // se o chute for diferente ao número secreto
+    alert('Você errou :(');
 }
